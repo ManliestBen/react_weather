e3d8bbb9c8ef2e2ea72dffb5ff92a8f6d8fadc9b
ManliestBen <benjamintmanley@gmail.com>
2019-10-01 15:15:16 -0500

    add component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from '../../components/Map/Map';
+import { getCurrentLatLng } from '../../services/geolocation';
 
 class App extends Component {
 
+  async componentDidMount() {
+    const {lat, lng} = await getCurrentLatLng();
+    console.log(lat, lng);
+  }
+  
   render() {
     return (
       <div className='App'>
